e6d186af1ec0b6f9fd6e04eaaeabc5dc45a74859
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-03-29 20:17:25 +0700

    menambah model dasar applikasi

--- register/models.py
+++ register/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class OrangTua:
+    nama = models.CharField(max_length=255)
+    tanggal_lahir = models.DateTimeField()
+    jenis_kelamin = models.CharField(max_length=1)
+    asal_sekolah = models.CharField(max_length=50)
+    alamat = models.CharField(max_length=255)
+
+
+
+class Siswa:
+    nama = models.CharField(max_length=255)
+    tanggal_lahir = models.DateTimeField()
+    jenis_kelamin = models.CharField(max_length=1)
+    asal_sekolah = models.CharField(max_length=50)
+    alamat = models.CharField(max_length=255)
+    ayah = models.ForeignKey(OrangTua, on_delete=models.CASCADE)
+    ibu = models.ForeignKey(OrangTua, on_delete=models.CASCADE)
+
+
+
+
